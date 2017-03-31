b058e113223181f495b09663f27585200ebb537a
John McIntosh II <jmci10@hotmail.com>
2017-03-30 21:32:53 -0400

    add more structure

---
+++ app/src/main/java/com/ver2point0/android/anchoragetours/Adapters/LocationAdapter.java
@@ -0,0 +1,13 @@
+package com.ver2point0.android.anchoragetours.Adapters;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import com.ver2point0.android.anchoragetours.Models.Location;
+
+public class LocationAdapter extends ArrayAdapter<Location> {
+
+    public LocationAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
